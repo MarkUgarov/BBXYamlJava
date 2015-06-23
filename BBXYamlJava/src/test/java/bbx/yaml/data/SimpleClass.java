e2cc6c7998d32e13ea156a9e22cd48154dbb5e97
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2015-06-23 21:57:30 +0200

    jackson tests added

---
+++ BBXYamlJava/src/test/java/bbx/yaml/data/SimpleClass.java
@@ -0,0 +1,14 @@
+package bbx.yaml.data;
+
+public class SimpleClass {
+
+    private String simpleString;
+
+    public String getSimpleString() {
+        return simpleString;
+    }
+
+    public void setSimpleString(String simpleString) {
+        this.simpleString = simpleString;
+    }
+}
