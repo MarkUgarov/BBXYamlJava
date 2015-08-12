df7b46fd783a0eddc09cb035477828e07a526c0d
MarkUgarov <m.ugarov86@googlemail.com>
2015-08-12 12:54:45 +0200

    added ignore
    ignore added

--- BBXYamlJava/src/main/java/yamlparse/Constants.java
+++ BBXYamlJava/src/main/java/yamlparse/Constants.java
@@ -25,7 +25,12 @@ public abstract class Constants {
     public static final String BBX_VERSION = "0.9.0";
     public static final String BBX_FILE_NAME= "biobox.yaml";
     public static final String[] VALID_FORMATS ={"fastq", "fasta"};
-    public static enum FastType implements Serializable {paired, unpaired, scaffold, contig}
+    public static class FastType implements Serializable {
+        public static enum FastQ{paired, unpaired}
+        public static enum FastA{scaffold, contig}
+        
+        }
+    }
    
     // for the YamlAssemblyEvaluationOutparser
     public static final String ASSEMBLY_EVALUATION_FILE_URL = "https://raw.githubusercontent.com/bioboxes/rfc/master/container/assembly-evaluation/input_schema.yaml";
