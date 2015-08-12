bbce9247c935e66e517765e11d067be0d2455139
MarkUgarov <m.ugarov86@googlemail.com>
2015-08-12 13:00:05 +0200

    ooops
    - accidentally updated to the wrong constants

--- BBXYamlJava/src/main/java/yamlparse/Constants.java
+++ BBXYamlJava/src/main/java/yamlparse/Constants.java
@@ -25,13 +25,8 @@ public abstract class Constants {
     public static final String BBX_VERSION = "0.9.0";
     public static final String BBX_FILE_NAME= "biobox.yaml";
     public static final String[] VALID_FORMATS ={"fastq", "fasta"};
-    public static class FastType implements Serializable {
-        public static enum FastQ{paired, unpaired}
-        public static enum FastA{scaffold, contig}
-        
-        }
-    }
-   
+    public static enum FastType implements Serializable {paired, unpaired, scaffold, contig};
+
     // for the YamlAssemblyEvaluationOutparser
     public static final String ASSEMBLY_EVALUATION_FILE_URL = "https://raw.githubusercontent.com/bioboxes/rfc/master/container/assembly-evaluation/input_schema.yaml";
     public static final String ASSEMBLY_EVALUATION_LOCAL_FILE_NAME= "AssemblyEvaluationData.yaml";
